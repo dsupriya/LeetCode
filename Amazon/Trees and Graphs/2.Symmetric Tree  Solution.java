e0f6880cad86e6249d0767d989ef7ad500cf6eac
dsupriya <supriyad570@gmail.com>
2020-09-28 16:08:35 +0530

    Update 2.Symmetric Tree  Solution.java

--- Amazon/Trees and Graphs/2.Symmetric Tree  Solution.java
+++ Amazon/Trees and Graphs/2.Symmetric Tree  Solution.java
@@ -37,3 +37,30 @@ class Solution {
         return (left.val == right.val) && isSysm(left.left,right.right) && isSysm(left.right,right.left);
     }
 }
+
+
+
+//iterative solution
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null)
+            return true;
+        LinkedList <TreeNode> q = new LinkedList <TreeNode>();
+        q.add(root);
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            TreeNode t1= q.poll();
+            TreeNode t2 = q.poll();
+            if(t1==null && t2==null) continue;
+            if(t1==null || t2== null) return false;
+            if(t1.val!=t2.val) return false;
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+        
+    }
+}
