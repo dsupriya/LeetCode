b4912d45316576661f88c40434fa39e73155c93c
dsupriya <supriyad570@gmail.com>
2022-02-18 23:14:13 +0530

    Time: 45 ms (29.62%), Space: 68.1 MB (6.98%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,31 +1,30 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        
+        LinkedList <Integer> cur = new LinkedList();
         List<List<Integer>> res = new LinkedList();
         
-        LinkedList <Integer> cur = new LinkedList();
-        
-        getCombinations(n,k,1,cur, res);
+        getCombinations(n,k,1,cur,res);
         return res;
         
+        
     }
-    
-    public void getCombinations(int n, int k, int start,LinkedList <Integer> cur,  List<List<Integer>> res)
+    public void getCombinations(int n, int k, int start,  LinkedList <Integer> cur,List<List<Integer>> res )
     {
         if(cur.size()==k)
         {
-            res.add(new LinkedList(cur));
+            LinkedList <Integer> ans = new LinkedList(cur);
+            res.add(ans);
             return;
         }
-        if(start>n)
-        {
+        if(start > n)
             return;
-        }
+        
         cur.add(start);
         getCombinations(n,k,start+1,cur,res);
         
         cur.removeLast();
         getCombinations(n,k,start+1,cur,res);
+        
         return;
     }
 }
