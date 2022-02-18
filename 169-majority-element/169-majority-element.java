fa93f10fcf06188353b206a78b7e07349a078d75
dsupriya <supriyad570@gmail.com>
2022-02-18 23:23:43 +0530

    Time: 3 ms (63.91%), Space: 56.5 MB (10.43%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        
+        int mid = nums.length/2;
+        
+        return nums[mid];
+        
+    }
+}
