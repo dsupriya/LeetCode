171298e84afae591e2c906af2b314ecff5fd9621
dsupriya <supriyad570@gmail.com>
2022-03-14 21:06:19 +0530

    Time: 21 ms (8.05%), Space: 44.3 MB (26.08%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String simplifyPath(String path) {
+        
+        Stack <String> s = new Stack();
+        
+        String []paths = path.split("//|/");
+        for(int i=0;i<paths.length;i++)
+        {
+            //System.out.println("this "+paths[i]);
+           if(paths[i].equals(".."))
+           {
+               if(!s.isEmpty())
+                   s.pop();
+           }
+           else if(paths[i].equals(".") || paths[i].equals(""))
+           {
+               continue;
+           }
+            else
+                s.push(paths[i]);
+        }
+        StringBuilder sb = new StringBuilder();
+        //System.out.println(s);
+        while(!s.isEmpty())
+        {
+            String word = "/" + s.pop();
+            sb.insert(0,word);
+        }
+        if(sb.isEmpty())
+            sb = sb.append("/");
+        return new String(sb);
+    }
+}
