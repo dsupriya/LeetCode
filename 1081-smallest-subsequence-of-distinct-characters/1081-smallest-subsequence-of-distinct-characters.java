daad7864d73a6775a5831745929ba440b19993a8
dsupriya <supriyad570@gmail.com>
2022-03-18 23:28:17 +0530

    Time: 2 ms (98.26%), Space: 40.1 MB (76.62%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        
+        int []last = new int[26];
+        boolean []seen = new boolean[26];
+        Stack <Integer> stack= new Stack();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            int c = s.charAt(i) - 'a';
+            last[c] = i;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            int c = s.charAt(i) - 'a';
+            if(seen[c])
+                continue;
+            while(!stack.isEmpty() && stack.peek()>c && i < last[stack.peek()])
+            {
+                seen[stack.pop()] = false;
+            }
+            stack.push(c);
+            seen[c] = true;
+        }
+        StringBuilder ans = new StringBuilder();
+        while(!stack.isEmpty())
+        {
+            char c = (char)(stack.pop()+'a');
+            ans.append(c);
+        }
+        return new String(ans.reverse());
+        
+        
+    }
+}
+/*idea is create array of last seen index of each elemnet
+if top of stack > cur and there is later occurence of top of the stack, then we can get rid of top of the stack
+*/
