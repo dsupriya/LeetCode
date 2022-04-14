500d81c658b6c9a0bb977258ef4484b7c5f8e874
dsupriya <supriyad570@gmail.com>
2022-04-14 22:58:08 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (84.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,11 +1,14 @@
 import java.util.*;
 class Solution {
     public int fib(int n) {
-        int []dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return fib_rec(n,dp);
+        //int []dp = new int[n+1];
+        //Arrays.fill(dp,-1);
+        //return fib_rec(n,dp);
+        return fib_itr(n);
+        
     }
     
+    /* recursive solution
     public int fib_rec(int n,int []dp)
     {
         if(dp[n]!=-1)
@@ -25,6 +28,23 @@ class Solution {
         int cur =  fib_rec(n-1,dp)+ fib_rec(n-2,dp);
         dp[n] = cur;
         
+        return cur;
+    }*/
+    
+    
+    public int fib_itr(int n)
+    {
+        if(n<=1)
+            return n;
+        int n_2 = 0;
+        int n_1 = 1;
+        int cur = 0;
+        for(int i=2;i<=n;i++)
+        {
+            cur = n_2 + n_1;
+            n_2 = n_1;
+            n_1 = cur;
+        }
         return cur;
     }
 }
