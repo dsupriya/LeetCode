8bf4519b413d4edf38c921d8eb2fef9c1c9063fb
dsupriya <supriyad570@gmail.com>
2022-07-06 20:10:00 +0530

    Time: 3 ms (90.43%), Space: 45.5 MB (44.65%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,23 +1,34 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap <Integer,Integer> hm  = new HashMap();
+        HashMap <Integer,Integer> hm = new HashMap(); // Value , Place
+        int n = nums.length;
+        int [] ans = new int[2];
         
-        int rem = 0;
-        int []ans = new int[2];
-        
-        for(int i=0;i<nums.length;i++)
-        { 
-            rem = target - nums[i];
+        for(int i=0;i<n;i++)
+        {
+            int rem = target - nums[i];
             if(hm.containsKey(rem))
             {
-                ans[0] = i;
-                ans[1] = hm.get(rem);
-                return ans;
+                ans [0] = hm.get(rem);
+                ans [1] = i;
+                break;
             }
             hm.put(nums[i],i);
-        
         }
         return ans;
         
     }
 }
+
+/* There is hashmap solution
+Space = o(n)
+time = o(n)
+
+With no space
+sort = nlogn
+space = o(1)
+
+Brute Force
+o(n2)
+
+*/
