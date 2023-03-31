10a8ab58d2471530ff597107887206ce31c72363
dsupriya <supriyad570@gmail.com>
2023-03-30 22:36:14 -0700

    Time: 1 ms, Memory: 43.3 MB - LeetHub

---
+++ single-number/single-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int n = nums.length;
+        int ans = 0 ;
+        
+        for(int i=0;i<n;i++)
+        {
+            ans = nums[i]^ans;
+        }
+        return ans;
+        
+    }
+}
